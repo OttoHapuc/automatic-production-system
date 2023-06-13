dc5c6ed140ce040517736c0538416782015b0ceb
OttoHapuc <dossantoshapuc@gmail.com>
2023-06-13 12:16:18 -0300

    refactory: ShiftWithStop component adjustment

--- src/components/production/machine/sessions/shiftWithStop/index.tsx
+++ src/components/production/machine/sessions/shiftWithStop/index.tsx
@@ -1,21 +1,20 @@
-'use client'
+"use client";
 
 import ProductionQuantity from "../utils/productionQuantity";
 
 export default function ShiftWithStop({ finishShift }: any) {
   return (
-    <>
-      <div className="py-2 flex-1 min-w-[375px]">
+      <div className="py-2 flex-1 min-w-[375px] space-y-4">
         <div className="flex flex-col gap-2">
           <span className="">Equipe I</span>
           <input
             type="text"
-            placeholder="Motivo da Stop"
+            placeholder="Motivo da Parada"
             className="h-7 w-full border-solid border-2 border-gray-400 rounded-md outline-none"
           />
         </div>
+        {finishShift && <ProductionQuantity />}
       </div>
-      {finishShift && <ProductionQuantity />}
-    </>
+
   );
 }
