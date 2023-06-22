c5bcbdaf8524197b45887231f2f35ac9e19180f8
OttoHapuc <dossantoshapuc@gmail.com>
2023-06-22 10:30:49 -0300

    style: DashBoard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 //import { redirect } from "next/navigation";
 
+import DashBoar from "@/components/DashBoar";
+
 export const metadata = {
   title: "DashBoard",
   description: "Sessão inicial",
 };
 
 export default function Home() {
-  return (
-    <>
-      por favor habra o menu no canto superior esquerdo e selecione o ambiente
-    </>
-  );
+  return <DashBoar />;
 }
---
+++ src/components/DashBoar/index.tsx
@@ -0,0 +1,86 @@
+export default function DashBoar() {
+  return (
+    <div className="p-4 w-full h-full flex justify-center">
+      <div className="w-4/5 h-full flex flex-wrap gap-12 overflow-auto pr-8">
+        <h1 className="text-9xl">
+          CONECTA<span className="text-lime-500">AB</span>
+        </h1>
+        <div className="flex flex-col gap-10">
+          <h2 className="text-5xl font-bold">
+            Homenagem Dia das Mães:
+            <i className="italic">
+              Vocês são os pilares que sustentam nossas vidas
+            </i>
+          </h2>
+          <p className="text-3xl">
+            No dia 14 de maio, celebramos o Dia das Mães e aqui no Grupo AB não
+            poderíamos deixar de homenagear todas as colaboradoras incríveis da
+            AB areias e Fazenda Sapucaia, que desempenham um papel importante em
+            suas famílias e em nossas empresas. Também prestamos uma homenagem
+            especial à querida dona Sirley Pedrosa Auricchio, que sempre será
+            lembrada pelo seu legado de amor e dedicação à família.
+          </p>
+          <h2 className="text-5xl font-bold">Parabéns a todas as mamães!</h2>
+          <div className="flex flex-col gap-8">
+            <button className="flex justify-around items-center p-4 text-5xl rounded-full bg-lime-500">
+              Confira o vídeo da AB areais clicando aqui!{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                />
+              </svg>
+            </button>
+            <button className="flex justify-around items-center p-4 text-5xl rounded-full bg-blue-400">
+              Confira o vídeo da AB areais clicando aqui!{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                />
+              </svg>
+            </button>
+          </div>
+          <h2 className="text-5xl font-bold">Parabéns a todas as mamães!</h2>
+          <p className="text-3xl">
+            A AB areias, por meio do Grupo AB Sol Energias Renováveis, está
+            investindo em projetos de geração de energia solar flutuante. Essa
+            tecnologia consiste na instalação de painéis solares sobre corpos
+            d'água, como represas, lagos e antigas cavas exauridas de mineração.
+            O Brasil possui condições favoráveis para esse tipo de geração de
+            energia, com altos índices de incidência solar e extensas áreas de
+            superfície de água. A empresa tem como objetivo alcançar a
+            autoprodução de energia renovável, permitindo que as empresas do
+            Grupo AB areias gerem e consumam sua própria energia, reduzindo
+            custos e contribuindo para o desenvolvimento sustentável. A
+            iniciativa busca alinhar-se aos propósitos de sustentabilidade e
+            inovação do grupo, reduzindo as emissões de gases de efeito estufa e
+            fomentando a cadeia produtiva de energias renováveis. A geração de
+            energia solar flutuante ainda é pouco explorada no Brasil, mas o
+            Grupo AB areias está buscando parcerias com empresas especializadas
+            em geração renovável de energia para criar fazendas solares
+            flutuantes nas áreas exauridas de suas minerações. Dessa forma, a
+            empresa demonstra seu compromisso com a sustentabilidade e o avanço
+            tecnológico no setor energético.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
