68c4d7cc314a1fc888b343729dc559fcaea017ab
OttoHapuc <dossantoshapuc@gmail.com>
2023-06-22 10:36:55 -0300

    refactor: DashBoard

--- src/components/DashBoar/index.tsx
+++ src/components/DashBoar/index.tsx
@@ -24,37 +24,9 @@ export default function DashBoar() {
           <div className="flex flex-col gap-8">
             <button className="flex justify-around items-center p-4 text-5xl rounded-full bg-lime-500">
               Confira o vídeo da AB areais clicando aqui!{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                />
-              </svg>
             </button>
             <button className="flex justify-around items-center p-4 text-5xl rounded-full bg-blue-400">
               Confira o vídeo da AB areais clicando aqui!{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                />
-              </svg>
             </button>
           </div>
           <h2 className="text-5xl font-bold">Parabéns a todas as mamães!</h2>
