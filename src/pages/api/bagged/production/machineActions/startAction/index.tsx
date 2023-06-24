0be8a86e375d585c76d4242d8ea485173bcf9c34
OttoHapuc <dossantoshapuc@gmail.com>
2023-06-24 15:36:26 -0300

    feat: machine actions

---
+++ src/pages/api/bagged/production/machineActions/startAction/index.tsx
@@ -0,0 +1,13 @@
+import { ErrorResponse } from "@/server/error";
+import authorizationToken from "@/server/middlewares/authorizationToken";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function MachineStart(req:NextApiRequest,res:NextApiResponse){
+    const { headers, body } = req;
+  try {
+    const user_id = await authorizationToken(headers);
+    res.status(201).json(`A maquina ${body.name} iniciou`);
+  } catch (error: ErrorResponse | any) {
+    return res.status(error.status).json({ message: error.message });
+  }
+}
